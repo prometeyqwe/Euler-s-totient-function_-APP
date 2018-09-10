be094b761fd1a8acb11ee024aebf7d0f36caf1af
prometeyqwe <kirilllov.evgeny@mail.ru>
2018-09-10 18:09:24 +0300

    Initial Commit

---
+++ Euler's totient function_ APP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Euler's totient function_ APP
+//
+//  Created by Евгений Киримов on 10.09.2018.
+//  Copyright © 2018 Evgeny Kirillov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
